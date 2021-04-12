22020960bea8e1793d73757e71d5aaf8a7bf29d7
SecretlySpy <professor6x@gmail.com>
2021-04-12 19:03:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/schoolify/Login.java
@@ -0,0 +1,36 @@
+package com.example.schoolify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Login extends AppCompatActivity {
+//Declaration Variables//
+    Button Reg_Stud, Reg_Teach;
+
+    @Override
+    //Content View//
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Reg_Stud = (Button) findViewById(R.id.ForStudents);
+        Reg_Stud.setOnClickListener(v -> openRegistration_For_Students());
+
+        Reg_Teach = (Button) findViewById(R.id.ForTeachers);
+        Reg_Teach.setOnClickListener(v -> openRegistration_For_Teachers());
+    }
+
+public void openRegistration_For_Students() {
+    Intent regstud = new Intent(this, Registration_For_Students.class);
+    startActivity(regstud);
+}
+public void openRegistration_For_Teachers() {
+        Intent regteach = new Intent(this, Registration_For_Teachers.class);
+    startActivity(regteach);
+}
+}
---
+++ app/src/main/java/com/example/schoolify/Registration_For_Teachers.java
@@ -0,0 +1,14 @@
+package com.example.schoolify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration_For_Teachers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration__for__teachers);
+    }
+}
