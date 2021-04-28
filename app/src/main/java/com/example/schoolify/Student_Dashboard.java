65793b1e38ce50c5d77f918da661c24bc4109e1c
SecretlySpy <professor6x@gmail.com>
2021-04-28 19:30:35 +0800

    Some changes

---
+++ app/src/main/java/com/example/schoolify/Student_Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.schoolify;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Student_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student__dashboard);
+    }
+}
